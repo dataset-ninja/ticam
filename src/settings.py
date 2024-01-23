c1d19cd0722934a0ce8c7042d6f5a9ab0408ad0a
peixed351 <peixed351@gmail.com>
2024-01-23 15:00:16 +0300

    release

--- src/settings.py
+++ src/settings.py
@@ -15,7 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "TiCaM: Real Images"
 PROJECT_NAME_FULL: str = "TICaM Real Images: A Time-of-Flight In-Car Cabin Monitoring Dataset"
-HIDE_DATASET = True  # set False when 100% sure about repo quality
+HIDE_DATASET = False  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
